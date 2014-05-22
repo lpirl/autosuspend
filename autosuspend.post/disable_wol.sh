c10bbf023c1a916e164b7ac662f65c476687b586
Lukas Pirl <git@lukas-pirl.de>
2014-05-22 21:51:24 +0200

    added workarounds for recend e1000[e] driver

--- autosuspend.post/disable_wol.sh
+++ autosuspend.post/disable_wol.sh
@@ -5,4 +5,13 @@
 # Otherwise, you may wonder why the box is powering on again after
 # power off.
 
+# We do not disable this for e1000* cards since they apply their
+# WOL options only upon shutdown and reboot:
+#
+# > WoL will be enabled on the system during the next shut down or reboot.
+# > For this driver version, in order to enable WoL, the e1000e driver
+# > must be loaded when shutting down or rebooting the system.
+#
+[ $(lsmod|grep -c "^e1000") -gt 0 ] && exit
+
 /sbin/ethtool -s eth0 wol g
--- autosuspend.pre/999-enable_wol.sh
+++ autosuspend.pre/999-enable_wol.sh
@@ -15,4 +15,21 @@
 #
 # check supported modes with `ethtool eth0|grep "Supports Wake"`
 
-/sbin/ethtool -s eth0 wol ug
+IF=eth0
+WAKE_ON=ug
+
+# fix for Intel e1000* cards:
+for (( i=0; i<${#WAKE_ON}; i++ )); do
+  TYPE=${WAKE_ON:$i:1}
+  [ $(ethtool $IF | grep -P "\tWake-on" | grep -c "$TYPE") -eq 0 ] && continue
+  MSG="You need to reboot your machine in order to make wake on LAN work.\n"
+  MSG+="Please refer to section 'Enabling Wake on LAN' at \n"
+  MSG+="http://www.cyberciti.biz/files/linux-kernel/Documentation/networking/e1000e.txt \n"
+  MSG+="for further details."
+  echo -e $MSG
+  (echo -e $MSG | sendmail s "autosyspendat $(hostname): reboot required" $(whoami)) \
+    > /dev/null 2>&1
+  break
+done
+
+/sbin/ethtool -s $IF wol $WAKE_ON
